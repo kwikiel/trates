90f3c89c5401c51bc23d8d4ed0208e05c6520a57
Kacper Wikieł <>
2019-06-02 07:50:17 +0200

    Compound Borrow Supply fix DB rebuild required after this

--- hello.py
+++ hello.py
@@ -27,7 +27,7 @@ def hello_world():
     headers = {"content-type": "application/json", "x-api-key": "KQUl7wEC9y8UTIU30zR71670L5iKpVl18XFD5Iqd"}
 
     r = requests.get("https://api.loanscan.io/v1/interest-rates", headers=headers)
-    compound2_dai = round(r.json()[1]["borrow"][3]["rate"]*100, 2)
+    compound2_dai = round(r.json()[1]["supply"][3]["rate"]*100, 2)
     dydx = round(r.json()[7]["supply"][1]["rate"]*100, 2)
     lever = round(r.json()[3]["supply"][1]["rate"]*100, 2)
 
