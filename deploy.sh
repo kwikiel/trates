558e09ecacc6adf99be29c4dfafa1bb94c3269ec
Kacper Wikieł <k.wikiel@gmail.com>
2019-10-09 16:59:58 +0200

    Deploy check status code

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,9 @@
 
 gcloud builds submit --tag gcr.io/airswap/trackato
 gcloud beta run deploy trackato --image gcr.io/airswap/trackato --platform managed
+
+response=$(curl --write-out %{http_code} --silent --output /dev/null https://trackato.com/)
+
+echo "Pings the endpoint to warmup. It should return 200 status code"
+echo $response
+
